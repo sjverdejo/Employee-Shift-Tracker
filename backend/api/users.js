7cbf2cfffd4acc5cb1c13d7275ccad01e06634f0
Sam Verdejo <sjverdejo@gmail.com>
2023-09-04 20:29:18 -0400

    Created router and completed GET routes for users

--- backend/api/users.js
+++ backend/api/users.js
@@ -1,1 +1,16 @@
+const usersRouter = require('express').Router()
+const db = require('../sql/users')
 
+usersRouter.get('/', async (req, res) => {
+  const users = await db.getAllUsers()
+
+  res.send(users)
+})
+
+usersRouter.get('/:id', async (req, res) => {
+  const user = await db.getUser(req.params.id)
+
+  res.send(user)
+})
+
+module.exports = usersRouter
