e23d2672815e1164f16661490560a094436f18b9
Sam Verdejo <sjverdejo@gmail.com>
2023-09-05 23:40:35 -0400

    Created all SELECT sql commands for shifts table

--- backend/build/database/shifts.js
+++ backend/build/database/shifts.js
@@ -10,14 +10,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 import sql from '../db.js';
 //Return all shifts
 const getAllShifts = () => __awaiter(void 0, void 0, void 0, function* () {
+    const shifts = yield sql `SELECT * FROM shifts;`;
+    return shifts;
+});
+//Return all shifts belonging to specific user
+const getAllUserShifts = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const shifts = yield sql `SELECT * FROM shifts WHERE employee = ${id};`;
+    return shifts;
 });
 //Return one shift 
 const getShift = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const shift = yield sql `SELECT * FROM shifts WHERE id = ${id};`;
+    return shift;
 });
-//Create a new shift for an employee
+//Create a new shift for an employee, only for scheduled start, scheduled end, and scheduled hours + employee assigned
 const createShift = (id, newShift) => __awaiter(void 0, void 0, void 0, function* () {
     const create = yield sql `INSERT INTO shifts (scheduled_start, scheduled_end, scheduled_hours, employee)
-    VALUES (${newShift.scheduled_start}, ${newShift.scheduled_end}, ${newShift.scheduled_hours}), (SELECT id FROM users WHERE id = ${id})`;
+    VALUES (${newShift.scheduled_start}, ${newShift.scheduled_end}, ${newShift.scheduled_hours}), (SELECT id FROM users WHERE id = ${id});`;
     return create;
 });
 export default { getAllShifts, getShift, createShift };
--- backend/src/database/shifts.ts
+++ backend/src/database/shifts.ts
@@ -1,14 +1,27 @@
-import internal from 'stream'
 import sql from '../db.js'
 
 //Return all shifts
 const getAllShifts = async () => {
+  const shifts = await sql
+    `SELECT * FROM shifts;`
 
+  return shifts
+}
+
+//Return all shifts belonging to specific user
+const getAllUserShifts = async (id: string) => {
+  const shifts = await sql
+    `SELECT * FROM shifts WHERE employee = ${id};`
+
+  return shifts
 }
 
 //Return one shift 
 const getShift = async (id: string) => {
+  const shift = await sql
+    `SELECT * FROM shifts WHERE id = ${id};`
 
+  return shift
 }
 
 //Interface for new shift
@@ -18,11 +31,11 @@ interface shiftObj {
   scheduled_hours: number
 }
 
-//Create a new shift for an employee
+//Create a new shift for an employee, only for scheduled start, scheduled end, and scheduled hours + employee assigned
 const createShift = async (id: string, newShift: shiftObj) => {
   const create = await sql
     `INSERT INTO shifts (scheduled_start, scheduled_end, scheduled_hours, employee)
-    VALUES (${newShift.scheduled_start}, ${newShift.scheduled_end}, ${newShift.scheduled_hours}), (SELECT id FROM users WHERE id = ${id})`
+    VALUES (${newShift.scheduled_start}, ${newShift.scheduled_end}, ${newShift.scheduled_hours}), (SELECT id FROM users WHERE id = ${id});`
 
   return create
 }
